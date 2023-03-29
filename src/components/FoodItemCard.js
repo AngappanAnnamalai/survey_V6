96926508fdf8f7a2a791c84b77a95c2bb115938d
Angappan Annamalai <angappan.annamalai@anu.edu.in>
2023-03-29 08:44:05 +0400

    re-modified

--- src/components/FoodItemCard.js
+++ src/components/FoodItemCard.js
@@ -1,79 +1,70 @@
 import styles from "./FoodItemCard.module.css";
 
-const FoodItemCard = ({
-  foodTagText,
-  foodTitle,
-  foodRatingText,
-  foodPrice,
-  foodDescription,
-  foodImage,
-  addButtonId,
-  addButtonCounterValue,
-}) => {
-  return (
-    <div className={styles.foodItemCard} id="food-item-card">
-      <div className={styles.foodInfo} id="food-info">
-        <div className={styles.foodAttributes} id="food-attributes">
-          <img className={styles.vectorIcon} alt="" src="/foodKind.svg" />
-          <div className={styles.foodTag} id="food-tag">
-            <p className={styles.foodTagText} id="food-tag-text">
-              {foodTagText}
-            </p>
-          </div>
-        </div>
-        <h3 className={styles.foodTitle} id="food-title">
-          {foodTitle}
-        </h3>
-        <div className={styles.foodRating} id="food-rating">
-          <img
-            className={styles.foodRatingStarIcon}
-            alt=""
-            id="food-rating-star"
-            src="/foodratingstaricon.svg"
-          />
-          <p className={styles.foodTagText} id="food-rating">
-            {foodRatingText}
+export default function FoodItemCard ({foodKind, foodTagText, foodTagColor, foodTitle, foodRatingText, foodPrice, foodDescription, foodImage,addButtonId, addButtonCounterText})  {
+    return  <div className={styles.foodItemCard} id="food-item-card">
+    <div className={styles.foodInfo} id="food-info">
+      <div className={styles.foodAttributes} id="food-attributes">
+          <svg className={styles.foodKindIcon} id="food-kind" width="19" height="18" viewBox="0 0 19 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path fillRule="evenodd" clipRule="evenodd" d="M13.5 2H5.5C3.84315 2 2.5 3.34315 2.5 5V13C2.5 14.6569 3.84315 16 5.5 16H13.5C15.1569 16 16.5 14.6569 16.5 13V5C16.5 3.34315 15.1569 2 13.5 2ZM5.5 0C2.73858 0 0.5 2.23858 0.5 5V13C0.5 15.7614 2.73858 18 5.5 18H13.5C16.2614 18 18.5 15.7614 18.5 13V5C18.5 2.23858 16.2614 0 13.5 0H5.5Z" fill={foodKind}/>
+          {foodKind == 'sienna' ? <path d="M8.62584 5.57348L5.32536 11.5144C4.95506 12.1809 5.43703 13 6.19951 13H12.8005C13.563 13 14.0449 12.1809 13.6746 11.5144L10.3742 5.57348C9.99315 4.88767 9.00685 4.88767 8.62584 5.57348Z" fill={foodKind}/> :
+          <path d="M13.5 9C13.5 11.2091 11.7091 13 9.5 13C7.29086 13 5.5 11.2091 5.5 9C5.5 6.79086 7.29086 5 9.5 5C11.7091 5 13.5 6.79086 13.5 9Z" fill={foodKind}/> }
+          </svg>
+        <div className={styles.foodTag} style={{backgroundColor: foodTagColor}} id="food-tag">
+          <p className={styles.foodTagText} id="food-tag-text">
+            {foodTagText}
           </p>
         </div>
-        <h6 className={styles.foodPrice} id="food-price">
-          {foodPrice}
-        </h6>
-        <p className={styles.foodDescription} id="food-description">
-          {foodDescription}
+      </div>
+      <h3 className={styles.foodTitle} id="food-title">
+        {foodTitle}
+      </h3>
+      <div className={styles.foodRating} id="food-rating">
+        <img
+          className={styles.foodRatingStarIcon}
+          alt=""
+          id="food-rating-star"
+          src="/foodratingstaricon.svg"
+        />
+        <p className={styles.foodTagText} id="food-rating">
+          {foodRatingText}
         </p>
       </div>
-      <div className={styles.foodImageButton} id="food-image-button">
-        <img
-          className={styles.foodImageIcon}
-          alt=""
-          id="food-image"
-          src={foodImage}
-        />
-        <div className={styles.addButtonCounter} id="Add-Button-Counter">
-          <img
-            className={styles.vectorIcon1}
-            alt=""
-            id="Subtract-Counter"
-            src="/subtracticon.svg"
-          />
-          <h4 className={styles.addButtonCounterText} id="Food-Item-Count">
-            {addButtonCounterValue}
-          </h4>
-          <img
-            className={styles.vectorIcon2}
-            alt=""
-            id="Add-Counter"
-            src="/plusicon.svg"
-          />
-        </div>
-        <button className={styles.addButton} disabled id={addButtonId}>
-          <p className={styles.addButtonText} id="add-button-text">
-            ADD +
-          </p>
-        </button>
-      </div>
+      <h6 className={styles.foodPrice} id="food-price">
+        {foodPrice}
+      </h6>
+      <p className={styles.foodDescription} id="food-description">
+        {foodDescription}
+      </p>
     </div>
-  );
-};
-
-export default FoodItemCard;
+    <div className={styles.foodImageButton} id="food-image-button">
+      <img
+        className={styles.foodImageIcon}
+        alt=""
+        id="food-image"
+        src={foodImage}
+      />
+      <div className={styles.addButtonCounter} id="Add-Button-Counter">
+        <img
+          className={styles.vectorIcon}
+          alt=""
+          id="Subtract-Counter"
+          src="/subtracticon.svg"
+        />
+        <h4 className={styles.addButtonCounterText} id="Food-Item-Count">
+          {addButtonCounterText}
+        </h4>
+        <img
+          className={styles.vectorIcon1}
+          alt=""
+          id="Add-Counter"
+          src="/plusicon.svg"
+        />
+      </div>
+      <button className={styles.addButton} disabled id={addButtonId}>
+        <p className={styles.addButtonText} id="add-button-text">
+          ADD +
+        </p>
+      </button>
+    </div>
+  </div>
+}
--- src/pages/FoodMenuPage.js
+++ src/pages/FoodMenuPage.js
@@ -2,6 +2,7 @@ import { useCallback } from "react";
 import FoodItemCard from "../components/FoodItemCard";
 import { useNavigate } from "react-router-dom";
 import styles from "./FoodMenuPage.module.css";
+import React from "react";
 
 const FoodMenuPage = () => {
   const navigate = useNavigate();
@@ -10,31 +11,52 @@ const FoodMenuPage = () => {
     navigate("/");
   }, [navigate]);
 
+  const [foodItemData, setFoodItemData ] = React.useState({})
+
+    React.useEffect(()=> {
+        const fetchData = async () => {
+            const result = await fetch('/.netlify/functions/fetchNotion')
+            result.json().then(json => {
+                setFoodItemData(json.results)
+                console.log(json.results);
+            })
+        }
+        fetchData()
+    }, [])
+    
+
   return (
     <main className={styles.foodMenuPage} id="main-page">
       <div className={styles.foodMenuContainer} id="food-menu-container">
-        <FoodItemCard
-          foodTagText="New"
-          foodTitle="Chicken Seekh Kebab 6pc"
-          foodRatingText="4.5 (145 ratings)"
-          foodPrice="₹295"
-          foodDescription="Served with mint chutney and salad. 6 Pieces, per box"
-          foodImage="/foodimage@2x.jpg"
-          addButtonId="add-item-init"
-          addButtonCounterValue="1"
-        />
-        <button
-          className={styles.backButton}
-          id="back-button"
-          onClick={onBackButtonClick}
-        >
-          <p className={styles.backButtonText} id="back-button-text">
-            Back
-          </p>
-        </button>
+            {Object.values(foodItemData).map(item => {
+                return <FoodItemCard
+                    key={item.properties.FoodID.number}
+                    foodKind={item.properties.FoodKind.select.name}
+                    foodTagText={item.properties.FoodTag.select.name}
+                    foodTagColor={item.properties.FoodTagColor.formula.string}
+                    foodTitle={item.properties.FoodTitle.title[0].plain_text}
+                    foodRatingText={`${item.properties.RatingValue.number} (${item.properties.RatingCount.number} ratings)`}
+                    foodPrice={`₹${item.properties.FoodPrice.number}`}
+                    foodDescription={item.properties.FoodDescription.rich_text[0].plain_text}
+                    foodImage={item.properties.FoodImage.url}
+                    addButtonId="add-item-init"
+                    addButtonCounterText="1"
+                />})
+            }
+            <button
+                className={styles.backButton}
+                id="back-button"
+                onClick={onBackButtonClick}
+                >
+                <p className={styles.backButtonText} id="back-button-text">
+                    Back
+                </p>
+                </button>
       </div>
     </main>
   );
 };
+
+
 
 export default FoodMenuPage;
