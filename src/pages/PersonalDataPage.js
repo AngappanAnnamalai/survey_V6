e615bc0075a1962f63eb7dd955b1191c84b8fbef
Angappan Annamalai <angappan.annamalai@anu.edu.in>
2023-04-04 13:45:38 +0400

    netlify redirect

--- src/pages/PersonalDataPage.js
+++ src/pages/PersonalDataPage.js
@@ -1,7 +1,19 @@
 import PersonalDataContainer from "../components/PersonalDataContainer";
 import styles from "./PersonalDataPage.module.css";
+// import React from "react";
 
 const PersonalDataPage = () => {
+
+    // React.useEffect(()=> {
+    //     const fetchData = async () => {
+    //         const result = await fetch('/.netlify/functions/fetchNotion')
+    //         result.json().then(json => {
+    //             console.log(json.results);
+    //         })
+    //     }
+    //     fetchData()
+    // }, [])
+
   return (
     <div className={styles.personalDataPage} id="form-page">
       <PersonalDataContainer />
